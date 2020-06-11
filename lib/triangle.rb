b551d96fe76664e50682657b6783c73237a8b467
Wenhao Guo <gwh1742072653@gmail.com>
2020-06-11 18:04:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,9 @@ class Triangle
     if (@side1 <= 0 ) || (@side2 <= 0 ) || (@side3 <= 0 )
       raise TriangleError
     elsif (@side1 + @side2 <= @side3) || (@side1 + @side3 <= @side2 ) || (@side2 + @sode3 <= @side1 )
-    raise TriangleError
+      raise TriangleError
+    else
+      
     
   end
 end
