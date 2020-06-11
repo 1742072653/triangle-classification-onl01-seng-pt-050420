40a679c327fbd299c7c1419a7feae5370b72f002
Wenhao Guo <gwh1742072653@gmail.com>
2020-06-11 18:02:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,20 +1,20 @@
 class Triangle
   
-def initialize(side1, side2, side3)
-  @side1 = side1
-  @side2 = side2
-  @side3 = side3
-end
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
 
-def kind
-  if (@side1 <= 0 ) || (@side2 <= 0 ) || (@side3 <= 0 )
+  def kind
+    if (@side1 <= 0 ) || (@side2 <= 0 ) || (@side3 <= 0 )
+  end
 end
-
 
 class TriangleError < StandardError
     
- end
+end
 
   
   
-end
+
